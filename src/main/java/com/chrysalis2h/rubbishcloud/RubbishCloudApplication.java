27d3d57801dd960cc3a5426eec160145e0dcb725
chrysalis2h <iamcoke@163.com>
2020-11-14 11:38:39 +0800

    :tada: 项目初始化

---
+++ src/main/java/com/chrysalis2h/rubbishcloud/RubbishCloudApplication.java
@@ -0,0 +1,13 @@
+package com.chrysalis2h.rubbishcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubbishCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubbishCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chrysalis2h/rubbishcloud/RubbishCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrysalis2h.rubbishcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubbishCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
