335da3573410f2d987b1052be5571846f2f59f73
chrysalis2h <iamcoke@163.com>
2021-11-29 16:03:54 +0800

    🔨 集成spring-cloud-config

---
+++ rubbish-cloud-config/src/main/java/com/chrysalis2h/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.chrysalis2h.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    /**
+     * /{application}/{profile}[/{label}]
+     * /{application}-{profile}.yml
+     * /{label}/{application}-{profile}.yml
+     * /{application}-{profile}.properties
+     * /{label}/{application}-{profile}.properties
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
--- rubbish-cloud-consumer/src/main/java/com/chrysalis2h/consumer/controller/RubbishController.java
+++ rubbish-cloud-consumer/src/main/java/com/chrysalis2h/consumer/controller/RubbishController.java
@@ -2,6 +2,7 @@ package com.chrysalis2h.consumer.controller;
 
 import com.chrysalis2h.consumer.client.RubbishClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +15,22 @@ public class RubbishController {
     @Autowired
     private RubbishClient rubbishClient;
 
+    @Value("${wahaha.key}")
+    private String hello;
+
     @RequestMapping("getRubbish")
     public String getRubbish(@RequestParam(required = false) String name) {
         if (StringUtils.isEmpty(name)) {
             return "~~~~~~~~~~~~~~~~~~~~~~";
         }
         return rubbishClient.getRubbish(name);
+    }
+
+    @RequestMapping("config")
+    public String getConfig(@RequestParam(required = false) String config) {
+        if (StringUtils.isEmpty(config)) {
+            return "~~~~~~~~~~~~~~~~~~~~~~";
+        }
+        return this.hello;
     }
 }
